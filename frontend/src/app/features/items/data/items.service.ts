e55dc7f5493eb0d4ec44ff4561b186221cf02910
ZenRadLad <xvod@protonmail.com>
2024-11-02 22:04:36 +0100

    move environment folder outside of app directory

--- frontend/src/app/features/items/data/items.service.ts
+++ frontend/src/app/features/items/data/items.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { Observable } from "rxjs";
 import { Item } from "./item.model";
-import { environment } from "../../../environments/environment";
+import { environment } from "../../../../environments/environment";
 
 @Injectable({
   providedIn: "root",
